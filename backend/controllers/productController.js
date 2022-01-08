f74ab6a285a46227f17f9d2c5c5b0af859d5789d
Frankie Luo <luo.jingy@northeastern.edu>
2022-01-07 16:53:22 -0800

    Added productList delete function.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,29 +1,44 @@
-import asyncHandler from 'express-async-handler' // asyncHandler is a middleware that is used to wrap async functions
-import Product from '../models/productModel.js'
+import asyncHandler from 'express-async-handler'; // asyncHandler is a middleware that is used to wrap async functions
+import Product from '../models/productModel.js';
 
 // @desc    Fetch all products
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({}) // pass an empty object (curly braces) to find all products
+  const products = await Product.find({}); // pass an empty object (curly braces) to find all products
 
-  res.json(products)
-})
+  res.json(products);
+});
 
 // @desc    Fetch single product
 // @route   GET /api/products/:id
 // @access  Public
 const getProductById = asyncHandler(async (req, res) => {
-  // CastError -> The problem is that its not a valid _id string. 
+  // CastError -> The problem is that its not a valid _id string.
   // It has to be either 12 byte binary string, or a 24 hex byte string.
-  const product = await Product.findById(req.params.id)
+  const product = await Product.findById(req.params.id);
 
   if (product) {
-    res.json(product)
+    res.json(product);
   } else {
-    res.status(404)
-    throw new Error('Product not found')
+    res.status(404);
+    throw new Error('Product not found');
   }
-})
+});
 
-export { getProducts, getProductById };
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: 'Product removed' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+export { getProducts, getProductById, deleteProduct };
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,45 +1,82 @@
-import axios from 'axios'
-import { 
-    PRODUCT_LIST_REQUEST,
-    PRODUCT_LIST_SUCCESS,
-    PRODUCT_LIST_FAIL,
-    PRODUCT_DETAILS_REQUEST,
-    PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_FAIL,
-} from "../constants/productConstants";
+import axios from 'axios';
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_FAIL,
+} from '../constants/productConstants';
 
 // this action creator will use the (dispatch) to dispatch or fire off the action to the reducer
 export const listProducts = () => async (dispatch) => {
-    try {
-        // what it will do is call in the reducer, and then the reducer will return the new state
-        dispatch({ type: PRODUCT_LIST_REQUEST });
+  try {
+    // what it will do is call in the reducer, and then the reducer will return the new state
+    dispatch({ type: PRODUCT_LIST_REQUEST });
 
-        const { data } = await axios.get('/api/products');
+    const { data } = await axios.get('/api/products');
 
-        dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
-    } catch (error) {
-        dispatch({ type: PRODUCT_LIST_FAIL,       
-            payload:
-            error.response && error.response.data.message
-              ? error.response.data.message
-              : error.message, 
-        });
-    }
-}
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
 export const listProductDetails = (id) => async (dispatch) => {
-    try {
-        dispatch({ type: PRODUCT_DETAILS_REQUEST });
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
-        const { data } = await axios.get(`/api/products/${id}`);
+    const { data } = await axios.get(`/api/products/${id}`);
 
-        dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
-    } catch (error) {
-        dispatch({ type: PRODUCT_DETAILS_FAIL,       
-            payload:
-            error.response && error.response.data.message
-              ? error.response.data.message
-              : error.message, 
-        });
-    }
-}
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const deleteProduct = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PRODUCT_DELETE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    await axios.delete(`/api/products/${id}`, config);
+
+    dispatch({
+      type: PRODUCT_DELETE_SUCCESS,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
